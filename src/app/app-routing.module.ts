05243168e53df13c952379df6960532f12edcc02
Elton Andrade <eltonsa75@hotmail.com>
2019-06-30 22:17:27 -0300

    commit -m "Criando as Rotas do Link"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { PorfolioComponent } from './porfolio/porfolio.component';
+
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+import { HomeComponent } from './home/home.component';
+import { EmpresaComponent } from './empresa/empresa.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { ContatoComponent } from './contato/contato.component';
+
+export const routes: Routes = [
+
+  {path: 'home', component: HomeComponent},
+  {path: 'empresa', component: EmpresaComponent},
+  {path: 'servicos', component: ServicosComponent},
+  {path: 'portolio', component: PorfolioComponent},
+  {path: 'contato', component: ContatoComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routes } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { HomeComponent } from './home/home.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { EmpresaComponent } from './empresa/empresa.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { PorfolioComponent } from './porfolio/porfolio.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +20,15 @@ import { EmpresaComponent } from './empresa/empresa.component';
     TopoComponent,
     HomeComponent,
     RodapeComponent,
-    EmpresaComponent
+    EmpresaComponent,
+    ServicosComponent,
+    PorfolioComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
